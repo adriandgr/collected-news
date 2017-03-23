67caafa437a4dd0c9a0da7bc8579b3b3d4198e54
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-23 15:55:09 -0700

    basic configuration of sequelize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const config = require('config');
 const app = express();
 const port = 8080;
+
+var env = process.env.NODE_ENV || 'development';
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
+  host: config.db.host,
+  dialect: config.db.dialect,
+  pool: config.db.pool
+});
+
+var User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync().then(function() {
+  return User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  });
+}).then(function(jane) {
+  console.log(jane.get({
+    plain: true
+  }));
+});
+
+
 
 // This is a test for a pull request
 
