4f5f55dab584424cc59ac8b1921cac1664f0912b
GEDDE5 <geddes.3754@gmail.com>
2017-03-23 15:07:58 -0700

    Removes comment

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// This is a test for a pull request
-
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
