e7098d9aa08d3ec693089ec55bea7cbe8a80cd63
Adrian Diaz <web.art@adriandiaz.ca>
2017-03-25 13:31:23 -0700

    source list

--- server/seeders/20170324215531-source.js
+++ server/seeders/20170324215531-source.js
@@ -6,7 +6,132 @@ module.exports = {
       link: 'http://feeds.foxnews.com/foxnews/world',
       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
-    }]),
+    }, {
+      name: 'Reuters: Technology News',
+      description: `Reuters.com is your source for breaking news, business, financial and
+        investing news, including personal finance and stocks.  Reuters is the leading global
+        provider of news, financial information and technology solutions to the world's media,
+        financial institutions, businesses and individuals.`,
+      link: 'http://feeds.reuters.com/reuters/technologyNews?format=xml',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'Reuters: Environment',
+      description: `Reuters.com is your source for breaking news, business, financial and
+        investing news, including personal finance and stocks.  Reuters is the leading global
+        provider of news, financial information and technology solutions to the world's media,
+        financial institutions, businesses and individuals.`,
+      link: 'http://feeds.reuters.com/reuters/environment?format=xml',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'Reuters: Most Read',
+      description: `Reuters.com is your source for breaking news, business, financial and
+        investing news, including personal finance and stocks.  Reuters is the leading global
+        provider of news, financial information and technology solutions to the world's media,
+        financial institutions, businesses and individuals.`,
+      link: 'http://feeds.reuters.com/reuters/MostRead?format=xml',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'NPR: News',
+      description: `NPR news, audio, and podcasts. Coverage of breaking stories, national and
+        world news, politics, business, science, technology, and extended coverage of major
+        national and world events.`,
+      link: 'http://www.npr.org/rss/rss.php?id=1001',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'NPR: Word',
+      description: `NPR world news, international art and culture, world business and
+        financial markets, world economy, and global trends in health, science and technology.
+        Subscribe to the World Story of the Day podcast and RSS feed.`,
+      link: 'http://www.npr.org/rss/rss.php?id=1004',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'NPR: Technology',
+      description: `Latest technology news and breakthroughs in technology, science, and
+        industry. Download the NPR Technology podcast and Technology RSS feed.`,
+      link: 'http://www.npr.org/rss/rss.php?id=1019',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'Globe and Mail: BC News',
+      description: ``,
+      link: 'http://www.theglobeandmail.com/news/british-columbia/?service=rss',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'Globe and Mail: Technology',
+      description: ``,
+      link: 'http://www.theglobeandmail.com/technology/tech-news/?service=rss',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    }, {
+      name: 'Globe and Mail: National',
+      description: ``,
+      link: 'http://www.theglobeandmail.com/news/national/?service=rss',
+      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+    },
+    ]),
   down: (queryInterface) =>
     queryInterface.bulkDelete('Sources', null, {}),
 };
+
+
+
+
+/*
+http://feeds.foxnews.com/foxnews/latest
+http://feeds.foxnews.com/foxnews/tech
+http://feeds.foxnews.com/foxnews/world
+
+http://feeds.reuters.com/reuters/technologyNews?format=xml
+
+http://www.slate.com/all.fulltext.all.rss
+
+http://rss.cbc.ca/lineup/topstories.xml
+
+http://rss.cbc.ca/lineup/canada.xml
+http://rss.cbc.ca/lineup/technology.xml
+http://rss.cbc.ca/lineup/canada-britishcolumbia.xml
+
+http://globalnews.ca/feed/rss2/
+
+http://www.metronews.ca/feeds.articles.news.vancouver.rss
+
+http://www.straight.com/content/rss
+
+http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml
+http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/technology/rss.xml
+
+http://theverge.com/rss/index.xml
+
+http://wired.com/?feed=rss2
+
+http://news.harker.org/?feed=rss2
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
