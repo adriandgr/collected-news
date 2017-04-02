50302af7c1f7b33b4d6c93b40181f2536faf6a4f
Adrian Diaz <web.art@adriandiaz.ca>
2017-04-01 19:38:03 -0700

    fix json syntax in gramaphone stopwords

--- server/seeders/20170324215531-source.js
+++ server/seeders/20170324215531-source.js
@@ -1,16 +1,84 @@
 
-module.exports = {
-  up: (queryInterface, Sequelize) =>
-    queryInterface.bulkInsert('Sources', [{
-      name: 'Fox News',
-      link: 'http://feeds.foxnews.com/foxnews/world',
-      createdAt: Sequelize.literal("(now() at time zone 'utc')"),
-      updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
-    }
-    ]),
-  down: (queryInterface) =>
-    queryInterface.bulkDelete('Sources', null, {}),
-};
+// module.exports = {
+//   up: (queryInterface, Sequelize) =>
+//     queryInterface.bulkInsert('Sources', [{
+//       name: 'Fox News',
+//       link: 'http://feeds.foxnews.com/foxnews/world',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Reuters: Technology News',
+//       description: `Reuters.com is your source for breaking news, business, financial and
+//         investing news, including personal finance and stocks.  Reuters is the leading global
+//         provider of news, financial information and technology solutions to the world's media,
+//         financial institutions, businesses and individuals.`,
+//       link: 'http://feeds.reuters.com/reuters/technologyNews?format=xml',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Reuters: Environment',
+//       description: `Reuters.com is your source for breaking news, business, financial and
+//         investing news, including personal finance and stocks.  Reuters is the leading global
+//         provider of news, financial information and technology solutions to the world's media,
+//         financial institutions, businesses and individuals.`,
+//       link: 'http://feeds.reuters.com/reuters/environment?format=xml',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Reuters: Most Read',
+//       description: `Reuters.com is your source for breaking news, business, financial and
+//         investing news, including personal finance and stocks.  Reuters is the leading global
+//         provider of news, financial information and technology solutions to the world's media,
+//         financial institutions, businesses and individuals.`,
+//       link: 'http://feeds.reuters.com/reuters/MostRead?format=xml',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'NPR: News',
+//       description: `NPR news, audio, and podcasts. Coverage of breaking stories, national and
+//         world news, politics, business, science, technology, and extended coverage of major
+//         national and world events.`,
+//       link: 'http://www.npr.org/rss/rss.php?id=1001',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'NPR: Word',
+//       description: `NPR world news, international art and culture, world business and
+//         financial markets, world economy, and global trends in health, science and technology.
+//         Subscribe to the World Story of the Day podcast and RSS feed.`,
+//       link: 'http://www.npr.org/rss/rss.php?id=1004',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'NPR: Technology',
+//       description: `Latest technology news and breakthroughs in technology, science, and
+//         industry. Download the NPR Technology podcast and Technology RSS feed.`,
+//       link: 'http://www.npr.org/rss/rss.php?id=1019',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Globe and Mail: BC News',
+//       description: ``,
+//       link: 'http://www.theglobeandmail.com/news/british-columbia/?service=rss',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Globe and Mail: Technology',
+//       description: ``,
+//       link: 'http://www.theglobeandmail.com/technology/tech-news/?service=rss',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     }, {
+//       name: 'Globe and Mail: National',
+//       description: ``,
+//       link: 'http://www.theglobeandmail.com/news/national/?service=rss',
+//       createdAt: Sequelize.literal("(now() at time zone 'utc')"),
+//       updatedAt: Sequelize.literal("(now() at time zone 'utc')"),
+//     },
+//     ]),
+//   down: (queryInterface) =>
+//     queryInterface.bulkDelete('Sources', null, {}),
+// };
 
 
 
