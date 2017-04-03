a45883a72b29ac1fb9a56560a83c9014395f5a69
GEDDE5 <geddes.3754@gmail.com>
2017-04-03 13:40:28 -0700

    Adds preliminary stats for number of different articles/sources/keywords

--- client/src/components/Analytics.vue
+++ client/src/components/Analytics.vue
@@ -2,6 +2,15 @@
   <div class="ui center aligned container">
     <SourceChart :data="allSources" > </SourceChart>
     <Trend v-for="trend in allTrends" :trend="trend"></Trend>
+    <div class="ui segment raised stat">
+      <h1 class="ui">{{ numArticles }} Articles</h1>
+    </div>
+    <div class="ui segment raised stat">
+      <h1 class="ui">{{ numSources }} Sources</h1>
+    </div>
+    <div class="ui segment raised stat">
+      <h1 class="ui">{{ numKeywords }} Keywords</h1>
+    </div>
   </div>
 
 </template>
@@ -17,27 +26,46 @@ export default {
   mounted () {
     this.retrieveTrends();
     this.setSources();
+    this.getKeywords();
   },
   computed: {
     ...mapGetters([
       'sources',
-      'trends'
+      'trends',
+      'keywords'
     ]),
     allSources () {
       return this.sources.results
     },
     allTrends() {
       return this.trends.results.data
+    },
+    numSources () {
+      return this.sources.results.length
+    },
+    numArticles () {
+      let n = 0;
+      this.sources.results.forEach(source => {
+        n += Number(source.total_articles);
+      });
+      return n;
+    },
+    numKeywords () {
+      return this.keywords.results.length;
     }
   },
   methods: {
     ...mapActions([
       'retrieveTrends',
-      'setSources'
+      'setSources',
+      'getKeywords'
     ])
   }
 }
 </script>
 
 <style scoped>
+  .stat {
+    width: 20%;
+  }
 </style>
