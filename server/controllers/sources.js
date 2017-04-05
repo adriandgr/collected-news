fa34e52b91f2ddede26c673e46b0db734b53ae7b
Adrian Diaz <web.art@adriandiaz.ca>
2017-04-05 12:36:09 -0700

    testing env

--- server/controllers/sources.js
+++ server/controllers/sources.js
@@ -1,51 +1,53 @@
 const Source = require('../models').Source;
 const Article = require('../models').Article;
-const {sequelize} = require('../models');
+const {sequelize, env} = require('../models');
+
 
 module.exports = {
   index(req, res) {
-    return sequelize.query(`
-      SELECT
-        "Sources".*,
-        COUNT("Articles"."id") AS "total_articles",
-        AVG("Articles"."sentiment") AS avg_sentiment
-      FROM
-        "Sources"
-      JOIN
-        "Articles"
-      ON
-        "Sources"."id" = "Articles"."sourceId"
-      GROUP BY
-        "Sources"."name",
-        "Sources"."id"
-      ORDER BY
-        avg_sentiment DESC
-    `)
-    //
-    .then(sourcesWithAnalytics => {
-      [sources, ] = sourcesWithAnalytics;
-      Article.all({
-        attributes: [
-          ['sourceId', 'id'],
-          [ sequelize.fn('max', sequelize.col('pubDate')), 'latest_article' ]
-        ],
-        where: ['"pubDate" is not null'],
-        group: [ 'sourceId' ]
-      })
-      .then(mostRecentArticlesForSources => {
-        sources.forEach(source => {
-          mostRecentArticlesForSources.forEach(instance => {
-            if(source.id === instance.dataValues.id) {
-              source.latestArticle = instance.dataValues.latest_article;
-            }
-          });
-        });
-        res.json(sources);
-      });
-    })
-    .catch(err => {
-      console.error(err);
-    });
+    res.json(env)
+    // return sequelize.query(`
+    //   SELECT
+    //     "Sources".*,
+    //     COUNT("Articles"."id") AS "total_articles",
+    //     AVG("Articles"."sentiment") AS avg_sentiment
+    //   FROM
+    //     "Sources"
+    //   JOIN
+    //     "Articles"
+    //   ON
+    //     "Sources"."id" = "Articles"."sourceId"
+    //   GROUP BY
+    //     "Sources"."name",
+    //     "Sources"."id"
+    //   ORDER BY
+    //     avg_sentiment DESC
+    // `)
+    // //
+    // .then(sourcesWithAnalytics => {
+    //   [sources, ] = sourcesWithAnalytics;
+    //   Article.all({
+    //     attributes: [
+    //       ['sourceId', 'id'],
+    //       [ sequelize.fn('max', sequelize.col('pubDate')), 'latest_article' ]
+    //     ],
+    //     where: ['"pubDate" is not null'],
+    //     group: [ 'sourceId' ]
+    //   })
+    //   .then(mostRecentArticlesForSources => {
+    //     sources.forEach(source => {
+    //       mostRecentArticlesForSources.forEach(instance => {
+    //         if(source.id === instance.dataValues.id) {
+    //           source.latestArticle = instance.dataValues.latest_article;
+    //         }
+    //       });
+    //     });
+    //     res.json(sources);
+    //   });
+    // })
+    // .catch(err => {
+    //   console.error(err);
+    // });
   },
   individual(req, res) {
     const id = req.params.id;
