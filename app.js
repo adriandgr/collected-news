fa3d36c433f26a01e3a81e16a09c506514a80b0c
Adrian Diaz <web.art@adriandiaz.ca>
2017-04-05 13:13:44 -0700

    path for views

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(logger('dev'));
+app.set('views', [path.join(__dirname, '/views'), path.join(__dirname, '/../views')]);
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
